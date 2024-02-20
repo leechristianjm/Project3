1712253691097e65853d447db8e8239cf1df8b41
leechristianjm <160551443+leechristianjm@users.noreply.github.com>
2024-02-20 13:35:17 -0800

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -3,9 +3,6 @@
 #include "GameConstants.h"
 #include <iostream>
 #include <string>
-class Pea;
-class Avatar;
-class Wall;
 using namespace std;
 
 Actor::Actor(StudentWorld* world, int ID, int startX, int startY, int startD, int startHealth): GraphObject(ID, startX, startY, startD) {
@@ -28,9 +25,6 @@ void Actor::setAttacked() {
 }
 void Actor::setAlive() {
     isAlive = true;
-}
-void Actor::setDirection(int dir) {
-    direction = dir;
 }
 void Actor::setAsObstacle() {
     isObstacle = true;
@@ -61,84 +55,82 @@ void Actor::moveIfPossible(int x, int y) {
         moveTo(x, y);
 }
 void Actor::doSomething() {}
+
 void Actor:: handleAttack() {}
 
-class Avatar: public Actor {
-    public:
-        Avatar(StudentWorld* world, int startX, int startY);
-        virtual ~Avatar() {}
-        int getPeas() const {
-            return numPeas;
-        }
-        void firePea();
-        virtual void doSomething() {
-            int key;
-            if (!isLiving())
-                return;
-            if (getWorld()->getKey(key)) {
-                switch (key) {
-                    case KEY_PRESS_ESCAPE:
-                        setDead();
-                        break;
-                    case KEY_PRESS_SPACE:
-                        if (numPeas > 0)
-                            firePea();
-                        break;
-                    case KEY_PRESS_LEFT:
-                        setDirection(left);
-                        moveIfPossible(getX()-1, getY());
-                        break;
-                    case KEY_PRESS_RIGHT:
-                        setDirection(right);
-                        moveIfPossible(getX()+1, getY());
-                        break;
-                    case KEY_PRESS_UP:
-                        setDirection(up);
-                        moveIfPossible(getX(), getY()+1);
-                        break;
-                    case KEY_PRESS_DOWN:
-                        setDirection(down);
-                        moveIfPossible(getX(), getY()-1);
-                        break;
-                    /*
-                     Then, the
-                     Avatar will try to move to the adjacent square in the direction it is facing
-                     if that square does not contain an obstruction: a marble that cannot be
-                     pushed, a wall, a pit, a robot, or a robot factory. If the player can move, it
-                     must update its location with the GraphObject class’s moveTo() method.
-                     */
-                    default: //Presses tab or enter
-                        break;
-                }
-            }
-        }
-    virtual void handleAttack() {
-        decreaseHealth(2);
-        if (getHealth() > 0)
-            getWorld()->playSound(SOUND_PLAYER_IMPACT);
-        else {
-            setDead();
-            getWorld()->playSound(SOUND_PLAYER_DIE);
+Avatar::Avatar(StudentWorld* world, int startX, int startY): Actor(world, IID_PLAYER, startX, startY, right, 20), numPeas(20) {}
+
+Avatar::~Avatar() {}
+
+int Avatar::getPeas() const {
+    return numPeas;
+}
+
+void Avatar:: doSomething() {
+    int key;
+    if (!isLiving())
+        return;
+    if (getWorld()->getKey(key)) {
+        switch (key) {
+            case KEY_PRESS_ESCAPE:
+                setDead();
+                //how do we indicate to game to restart the level with all the objects in the original position. Would I need to keep track of the original x,y of each object in StudentWorld.cpp?
+                break;
+            case KEY_PRESS_SPACE:
+                if (numPeas > 0)
+                    firePea();
+                break;
+            case KEY_PRESS_LEFT:
+                setDirection(left);
+                moveIfPossible(getX()-1, getY());
+                break;
+            case KEY_PRESS_RIGHT:
+                setDirection(right);
+                moveIfPossible(getX()+1, getY());
+                break;
+            case KEY_PRESS_UP:
+                setDirection(up);
+                moveIfPossible(getX(), getY()+1);
+                break;
+            case KEY_PRESS_DOWN:
+                setDirection(down);
+                moveIfPossible(getX(), getY()-1);
+                break;
+                /*
+                 Then, the
+                 Avatar will try to move to the adjacent square in the direction it is facing
+                 if that square does not contain an obstruction: a marble that cannot be
+                 pushed, a wall, a pit, a robot, or a robot factory. If the player can move, it
+                 must update its location with the GraphObject class’s moveTo() method.
+                 */
+            default: //Presses tab or enter
+                break;
         }
     }
-    private:
-        int numPeas;
-};
+    
+}
 
-class Wall: public Actor {
-    public:
-        Wall(StudentWorld* world, int startX, int startY): Actor(world, IID_WALL, startX, startY, none, 0) {
-            setAsObstacle();
-        }
-        virtual ~Wall() {}
-};
+void Avatar::handleAttack() { //avatar collides with smth
+    decreaseHealth(2);
+    if (getHealth() > 0)
+        getWorld()->playSound(SOUND_PLAYER_IMPACT);
+    else {
+        setDead();
+        getWorld()->playSound(SOUND_PLAYER_DIE);
+    }
+}
+
+Wall::Wall(StudentWorld* world, int startX, int startY): Actor(world, IID_WALL, startX, startY, none, 0) {
+    setAsObstacle();
+}
+
+Wall::~Wall() {}
 
 class Pea: public Actor {
     public:
         Pea(StudentWorld* world, int startX, int startY, int direction): Actor(world, IID_PEA, startX, startY, direction, 1) {}
         virtual ~Pea() {}
 };
-Avatar::Avatar(StudentWorld* world, int startX, int startY): Actor(world, IID_PLAYER, startX, startY, right, 20), numPeas(20) {}
 
 void Avatar::firePea() {
     int peaX = getX();
@@ -162,10 +154,19 @@ void Avatar::firePea() {
         cout << "Work on adding pea here";
         Pea* newPea = new Pea(getWorld(), peaX, peaY, getDirection());
         getWorld()->addObject(newPea);
-        //would I add peas by creating a pointer to a new pea inside Avatar and then sending that to a function in StudentWorld  that appends objects to the end of the list of obejcts? same for robot factoryy dispensing robots?
     }
     getWorld()->playSound(SOUND_PLAYER_FIRE);
 }
+//is it colliding - look at #7
+//collision method inside pea
+//studentworld - if colliding / pass peas current position and traverse current array - return ptr to other obj not pea
+//inside actor.cpp collide method -> decrease health
+
+//pea hits object - custom things / in actor have method in actor and inherited class overrides method 
+
+
+
+
 //setasobstacle for pit, robot factory and robot base class
 //exit: setVisible(false)
 //if I check that a robot can attack a player, how do I set player's isAttacked to true? Dont know how to access player from robot
@@ -178,3 +179,4 @@ void Avatar::firePea() {
 //error with adding peas
 //Never make any class’s data members public or protected. You may make class constants public, protected or private. ??
 //If only some objects have hit points like Avatar and robots, should I put health points in the base Actor class or repeat having health points in both avatar and robot classe s
+
